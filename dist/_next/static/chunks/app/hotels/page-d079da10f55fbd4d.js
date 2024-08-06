5daeb7d34c8740eada85bb5d06df02f545b6514b
Ed Frankowski <frankowskiedward@gmail.com>
2024-08-06 16:11:06 +0300

    update build

--- dist/_next/static/chunks/app/hotels/page-d079da10f55fbd4d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4887],{},function(n){n.O(0,[1544,5250,1749,4685,1565,7612,8101,2971,8069,1744],function(){return n(n.s=98135)}),_N_E=n.O()}]);
