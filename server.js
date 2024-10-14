fa76bd70203bd8c2ee9c0aa2df41bedc3ded291c
Ed Frankowski <frankowskiedward@gmail.com>
2024-10-14 21:09:59 +0300

    update seo

--- server.js
+++ server.js
@@ -272,277 +272,322 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 const redirects =  [
     {
-        source: "/_error",
+        source: "/_error/",
         destination: "/404/",
         permanent: true,
     },
     {
-        source: "/port",
+        source: "/port/",
         destination: "/",
         permanent: true,
     },
     {
-        source: "/contact",
+        source: "/contact/",
         destination: "/404/",
         permanent: true,
     },
     {
-        source: "/contacts",
+        source: "/contacts/",
         destination: "/404/",
         permanent: true,
     },
     {
-        source: "/dogovor-bescontact",
+        source: "/dogovor-bescontact/",
         destination: "/non-contact-check-in/",
         permanent: false,
     },
     {
-        source: "/partners",
+        source: "/partners/",
         destination: "/places-to-go/",
         permanent: false,
     },
     {
-        source: "/dogovor",
+        source: "/dogovor/",
         destination: "/rules-of-accommodation/",
         permanent: true,
     },
     {
-        source: "/dopuslugi",
+        source: "/dopuslugi/",
         destination: "/additional-services/",
         permanent: true,
     },
     {
-        source: "/loyalty",
+        source: "/loyalty/",
         destination: "/loyalty-programs/",
         permanent: true,
     },
     {
-        source: "/payment",
+        source: "/payment/",
         destination: "/payment-and-return/",
         permanent: true,
     },
     {
-        source: "/policy",
+        source: "/policy/",
         destination: "/privacy-policy/",
         permanent: true,
     },
     {
-        source: "/rekvizity",
+        source: "/rekvizity/",
         destination: "/requisites/",
         permanent: true,
     },
     {
-        source: "/booking",
+        source: "/booking/",
         destination: "/hotels/spb/",
         permanent: true,
     },
     {
-        source: "/dolgosrochnoeprojivanie",
+        source: "/dolgosrochnoeprojivanie/",
         destination: "/hotels/spb/",
         permanent: true,
     },
     {
-        source: "/hotels",
+        source: "/hotels/",
         destination: "/hotels/spb/",
         permanent: true,
     },
     {
-        source: "/hotelsmoscow",
+        source: "/hotelsmoscow/",
         destination: "/hotels/msk/",
         permanent: true,
     },
     {
-        source: "/port-comfort-by-moyka",
+        source: "/port-comfort-by-moyka/",
         destination: "/hotels/spb/moyka/",
         permanent: true,
     },
     {
-        source: "/port-comfort-by-old-nevsky",
+        source: "/port-comfort-by-old-nevsky/",
         destination: "/hotels/spb/old-nevskiy/",
         permanent: true,
     },
     {
-        source: "/port-comfort-by-smolnyy",
+        source: "/port-comfort-by-smolnyy/",
         destination: "/hotels/spb/smolnyy/",
         permanent: true,
     },
     {
-        source: "/port-comfort-by-the-gostiny-dvor",
+        source: "/port-comfort-by-the-gostiny-dvor/",
         destination: "/hotels/spb/gostiniy-dvor/",
         permanent: true,
     },
     {
-        source: "/port-comfort-by-the-griboedov",
+        source: "/port-comfort-by-the-griboedov/",
         destination: "/hotels/spb/griboedov/",
         permanent: true,
     },
     {
-        source: "/port-comfort-on-ligovskiy",
+        source: "/port-comfort-on-ligovskiy/",
         destination: "/hotels/spb/ligovskiy/",
         permanent: true,
     },
     {
-        source: "/port-comfort-on-moyka",
+        source: "/port-comfort-on-moyka/",
         destination: "/hotels/spb/moyka/",
         permanent: true,
     },
     {
-        source: "/port-comfort-on-pokrovka",
+        source: "/port-comfort-on-pokrovka/",
         destination: "/hotels/msk/pokrovka/",
         permanent: true,
     },
     {
-        source: "/sennaya_square",
+        source: "/sennaya_square/",
         destination: "/hotels/spb/sennaya-square/",
         permanent: true,
     },
     {
-        source: "/booking?hotel_id=20176&room-type=145132",
+        source: "/booking?hotel_id=20176&room-type=145132/",
         destination: "/hotels/spb/gostiniy-dvor/",
         permanent: true,
     },
     {
-        source: "/booking?hotel_id=10970",
+        source: "/booking?hotel_id=10970/",
         destination: "/hotels/spb/ligovskiy/",
         permanent: true,
     },
     {
-        source: "/book-a-room/ligovskiy",
+        source: "/book-a-room/ligovskiy/",
         destination: "/hotels/spb/ligovskiy/",
         permanent: true,
     },
     {
-        source: "/book-a-room/gostiniy-dvor",
+        source: "/book-a-room/gostiniy-dvor/",
         destination: "/hotels/spb/gostiniy-dvor/",
         permanent: true,
     },
     {
-        source: "/book-a-room/pokrovka",
+        source: "/book-a-room/pokrovka/",
         destination: "/hotels/msk/pokrovka/",
         permanent: true,
     },
     {
-        source: "/book-a-room/moyka",
+        source: "/book-a-room/moyka/",
         destination: "/hotels/spb/moyka/",
         permanent: true,
     },
     {
-        source: "/book-a-room/old-nevskiy",
+        source: "/book-a-room/old-nevskiy/",
         destination: "/hotels/spb/old-nevskiy/",
         permanent: true,
     },
     {
-        source: "/book-a-room/smolnyy",
+        source: "/book-a-room/smolnyy/",
         destination: "/hotels/spb/smolnyy/",
         permanent: true,
     },
     {
-        source: "/book-a-room/griboedov",
+        source: "/book-a-room/griboedov/",
         destination: "/hotels/spb/griboedov/",
         permanent: true,
     },
     {
-        source: "/book-a-room/sennaya-square",
+        source: "/book-a-room/sennaya-square/",
         destination: "/hotels/spb/sennaya-square/",
         permanent: true,
     },
     {
-        source: "/book-a-room/angliyskiy",
+        source: "/book-a-room/angliyskiy/",
         destination: "/hotels/spb/angliyskiy/",
         permanent: true,
     },
     {
-        source: "/book-a-room?hotel_id=20176",
+        source: "/book-a-room?hotel_id=20176/",
         destination: "/hotels/spb/gostiniy-dvor/",
         permanent: true,
     },
     {
-        source: "/book-a-room?hotel_id=32789",
+        source: "/book-a-room?hotel_id=32789/",
         destination: "/hotels/msk/pokrovka/",
         permanent: true,
     },
     {
-        source: "/book-a-room?hotel_id=21668",
+        source: "/book-a-room?hotel_id=21668/",
         destination: "/hotels/spb/old-nevskiy/",
         permanent: true,
     },
     {
-        source: "/book-a-room?hotel_id=42043",
+        source: "/book-a-room?hotel_id=42043/",
         destination: "/hotels/spb/angliyskiy/",
         permanent: true,
     },
     {
-        source: "/book-a-room?hotel_id=20776",
+        source: "/book-a-room?hotel_id=20776/",
         destination: "/hotels/spb/griboedov/",
         permanent: true,
     },
     {
-        source: "/book-a-room?hotel_id=33783",
+        source: "/book-a-room?hotel_id=33783/",
         destination: "/hotels/spb/sennaya-square/",
         permanent: true,
     },
     {
-        source: "/book-a-room?hotel_id=10970",
+        source: "/book-a-room?hotel_id=10970/",
         destination: "/hotels/spb/ligovskiy/",
         permanent: true,
     },
     {
-        source: "/book-a-room?hotel_id=27469",
+        source: "/book-a-room?hotel_id=27469/",
         destination: "/hotels/spb/moyka/",
         permanent: true,
     },
     {
-        source: "/book-a-room?hotel_id=27859",
+        source: "/book-a-room?hotel_id=27859/",
         destination: "/hotels/spb/smolnyy/",
         permanent: true,
     },
     {
-        source: "/spb/griboedov",
+        source: "/spb/griboedov/",
         destination: "/hotels/spb/griboedov/",
         permanent: true,
     },
     {
-        source: "/spb/smolnyy",
+        source: "/spb/smolnyy/",
         destination: "/hotels/spb/smolnyy/",
         permanent: true,
     },
     {
-        source: "/spb/old-nevskiy",
+        source: "/spb/old-nevskiy/",
         destination: "/hotels/spb/old-nevskiy/",
         permanent: true,
     },
     {
-        source: "/msk/pokrovka",
+        source: "/msk/pokrovka/",
         destination: "/hotels/msk/pokrovka/",
         permanent: true,
     },
     {
-        source: "/spb/moyka",
+        source: "/spb/moyka/",
         destination: "/hotels/spb/moyka/",
         permanent: true,
     },
     {
-        source: "/spb/ligovskiy",
+        source: "/spb/ligovskiy/",
         destination: "/hotels/spb/ligovskiy/",
         permanent: true,
     },
     {
-        source: "/spb/sennaya-square",
+        source: "/spb/sennaya-square/",
         destination: "/hotels/spb/sennaya-square/",
         permanent: true,
     },
     {
-        source: "/spb/gostiniy-dvor",
+        source: "/spb/gostiniy-dvor/",
         destination: "/hotels/spb/gostiniy-dvor/",
         permanent: true,
     },
     {
-        source: "/spb/angliyskiy",
-        destination: "/hotels/spb/angliyskiy/",
+        source: "/spb/angliyskiy/",
+        destination: "/hotels/spb/mariinsky/",
         permanent: true,
     },
     {
         source: "/hotels/spb/angliyskiy/",
+        destination: "/hotels/spb/mariinsky/",
+        permanent: true,
+    },
+    {
+        source: "/spb/moyka/",
+        destination: "/hotels/spb/moyka/",
+        permanent: true,
+    },
+    {
+        source: "/spb/gostiniy-dvor/",
+        destination: "/hotels/spb/gostiniy-dvor/",
+        permanent: true,
+    },
+    {
+        source: "/spb/sennaya-square/",
+        destination: "/hotels/spb/sennaya-square/",
+        permanent: true,
+    },
+    {
+        source: "/msk/pokrovka/",
+        destination: "/hotels/msk/pokrovka/",
+        permanent: true,
+    },
+    {
+        source: "/spb/old-nevskiy/",
+        destination: "/hotels/spb/old-nevskiy/",
+        permanent: true,
+    },
+    {
+        source: "/spb/smolnyy/",
+        destination: "/hotels/spb/smolnyy/",
+        permanent: true,
+    },
+    {
+        source: "/spb/griboedov/",
+        destination: "/hotels/spb/griboedov/",
+        permanent: true,
+    },
+    {
+        source: "/spb/ligovskiy/",
+        destination: "/hotels/spb/ligovskiy/",
+        permanent: true,
+    },
+    {
+        source: "/spb/mariinsky/",
         destination: "/hotels/spb/mariinsky/",
         permanent: true,
     },
@@ -576,8 +621,9 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res) => {
-    res.redirect('/404/');
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'dist', '404', 'index.html'));
 });
 
 app.post('/send-transfer-message', async (req, res) => {
