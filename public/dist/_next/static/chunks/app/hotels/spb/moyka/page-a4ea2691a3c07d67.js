084784d3830138f8471e4dcf93bc0cc1a14d3479
Ed Frankowski <frankowskiedward@gmail.com>
2025-03-14 14:17:07 +0300

    update hotels page warning

---
+++ public/dist/_next/static/chunks/app/hotels/spb/moyka/page-a4ea2691a3c07d67.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2113],{},function(n){n.O(0,[5250,1749,4685,6345,2943,4667,2971,8069,1744],function(){return n(n.s=55387)}),_N_E=n.O()}]);
--- public/dist/_next/static/chunks/app/places-to-go/page-5817d3b365e8d7ca.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3110],{},function(n){n.O(0,[5250,1749,4685,6345,7310,2971,8069,1744],function(){return n(n.s=10836)}),_N_E=n.O()}]);
